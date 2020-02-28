1154cb6b36e8de4365684a219d1c21eaf578cedb
Fernando Soares <fernandolinharessoares@gmail.com>
2020-02-28 13:48:34 -0300

    Adicionando validações de atributos

--- api-produtos/src/main/java/com/exemplo/rest/produtos/models/Produto.java
+++ api-produtos/src/main/java/com/exemplo/rest/produtos/models/Produto.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Produto implements Serializable {
@@ -19,8 +21,11 @@ public class Produto implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank
 	private String nome;
+	@NotNull
 	private BigDecimal quantidade;
+	@NotNull
 	private BigDecimal valor;
 
 	public Long getId() {
